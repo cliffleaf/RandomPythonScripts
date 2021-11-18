f6cb5d8e15c7239edc9cdc9c0e91dc1666cb3e9a
cliffleaf <53232169+cliffleaf@users.noreply.github.com>
2021-11-19 11:53:32 +1300

    Update sort_4_numbers_withoutlist.py

--- sort_4_numbers_withoutlist/sort_4_numbers_withoutlist.py
+++ sort_4_numbers_withoutlist/sort_4_numbers_withoutlist.py
@@ -1,1 +1,20 @@
-
+def sort_four_nums(num1, num2, num3, num4):
+    
+    # round 1
+    min_round_1 = min(num1, num2)
+    max_round_1 = max(num1, num2)
+    
+    # round 2
+    smallest = min(min_round_1, num3, num4)
+    temp_max = max(min_round_1, num3, num4)
+    temp_middle = min_round_1 + num3 + num4 - smallest - temp_max
+    
+    # round 3
+    largest = max(max_round_1, temp_max, temp_middle)
+    second_smallest = min(max_round_1, temp_max, temp_middle)
+    second_largest = max_round_1 + temp_max + temp_middle - second_smallest - largest
+    
+    
+    # final
+    
+    print(smallest, second_smallest, second_largest,largest)
